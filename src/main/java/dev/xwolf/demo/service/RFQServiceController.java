9d3084c51bde8a9f0048d6ed72d758ad1e9655ef
robinwu2008 <robinwu2008@126.com>
2015-11-23 13:13:37 +0800

    updated
    xxxxxxx

--- src/main/java/dev/xwolf/demo/service/RFQServiceController.java
+++ src/main/java/dev/xwolf/demo/service/RFQServiceController.java
@@ -1,5 +1,34 @@
 package dev.xwolf.demo.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class RFQServiceController {
+	
+	
+	@RequestMapping(value = "/pn_list")
+	@ResponseBody
+	public List<String> pn(String pn){
+		
+		List<String> list=new ArrayList<String>();
+		for(int i=0;i<5;i++){
+			list.add(pn+"-"+i);
+		}
+		return list;
+	}
+	
+	
+	@RequestMapping(value = "/pn/list/")
+	@ResponseBody
+	public List<Map> product(){
+		return null;
+	}
+	
 
 }
